34755c134789ea7b57fb0cc1237e2d1c05fa4479
Jaron Armiger <jaron.armiger@gmail.com>
2023-03-01 14:21:21 -0800

    incorporate ScrollTrigger

--- src/main.js
+++ src/main.js
@@ -1,3 +1,38 @@
 import './styles/style.css'
+import Lenis from '@studio-freight/lenis'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 console.log('allure')
+
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+  direction: 'vertical', // vertical, horizontal
+  gestureDirection: 'vertical', // vertical, horizontal, both
+  smooth: true,
+  mouseMultiplier: 1,
+  smoothTouch: false,
+  touchMultiplier: 2,
+  infinite: false,
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+gsap.registerPlugin(ScrollTrigger)
+
+gsap.to('.section__01', {
+  yPercent: 100,
+  ease: 'none',
+  scrollTrigger: {
+    trigger: '.section__02',
+    start: 'top bottom',
+    end: 'top top',
+    scrub: true,
+  },
+})
